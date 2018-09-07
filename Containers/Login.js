743c60098194a0c946a7716aa5d19eef5c3f9f32
Boxuan Lu <547181293@qq.com>
2018-09-07 11:13:03 +1000

    add login logo

--- Containers/Login.js
+++ Containers/Login.js
@@ -1,11 +1,16 @@
 import React, {Component} from "react";
-import {ScrollView, Text, TextInput, View, Button} from "react-native";
+import {ScrollView, Text, TextInput, View, Button, Image} from "react-native";
 import OpenShifts from './OpenShifts';
+import { Styles } from './Styles.js';
 
 class Login extends React.Component {
     render() {
         return (
             <ScrollView style={{padding: 20}}>
+
+                <Image source={require("../res/Logo-100.jpg")}
+                style={{height: 100, width: 200, marginLeft: 50, marginTop: 100}}/>
+
                 <Text style={{fontSize: 27}}>Login</Text>
                 <TextInput placeholder="Username"/>
                 <TextInput placeholder="Password"/>
